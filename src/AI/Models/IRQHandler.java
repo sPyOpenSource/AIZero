43c799e364281bc46b87bb64d105c6f3e5b88738
X. Wang <xuyi@tetra.nl>
2025-01-24 19:03:19 +0100

    save

--- src/AI/Models/IRQHandler.java
+++ src/AI/Models/IRQHandler.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package AI.Models;
 
 import AI.AIZeroLogic;
@@ -13,21 +10,21 @@ import jx.zero.FirstLevelIrqHandler;
  */
 public class IRQHandler implements FirstLevelIrqHandler {
         
-        private final int irq;
-        
-        public IRQHandler(int irq){
-            this.irq = irq;
+    private final int irq;
+
+    public IRQHandler(int irq){
+        this.irq = irq;
+    }
+
+    @Override
+    public void interrupt() {
+        for(FirstLevelIrqHandler handler:AIZeroLogic.handlers[irq]){
+            if(handler != null)
+                handler.interrupt();
         }
-        
-        @Override
-        public void interrupt() {
-            for(FirstLevelIrqHandler handler:AIZeroLogic.handlers[irq]){
-                if(handler != null)
-                    handler.interrupt();
-            }
-        }
-        
-        public static boolean OK(int irq){
-            return AIZeroLogic.handlers[irq][1] == null;
-        }
+    }
+
+    public static boolean OK(int irq){
+        return AIZeroLogic.handlers[irq][1] == null;
+    }
 }
